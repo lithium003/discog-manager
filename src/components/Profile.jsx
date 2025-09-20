7cfbd0d272e127098db69ee66e30ad56d7b6d849
Mitch <mitch.hare03@gmail.com>
2025-09-20 17:18:00 +1000

    Install React and vitejs/plugin-react, fix lint config settings, create and import example component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,8 @@
+export const Profile = () => {
+  return (
+    <>
+      <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />
+      <p>Name: Katherine Johnson</p>
+    </>
+  );
+};
